d82d02c4294688b736d1211aa6918a882e0ef8e6
rainbowpieeee <catherinestolbova@yandex.ru>
2024-01-08 16:31:34 +0500

    добавил логику авторизации и ограничений доступа

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,13 +1,42 @@
 import { defineStore } from "pinia";
+import { GET, POST, DELETE, setToken, removeToken } from "../services";
 
 export const useAuthStore = defineStore("auth", {
   state: () => ({
     userInfo: {},
+    auth: false,
   }),
-  getters: {},
+  getters: {
+    authorized(state) {
+      return state.userInfo.id;
+    },
+  },
   actions: {
-    setUserInfo(user) {
-      this.userInfo = user;
+    async setUserInfo() {
+      const { data } = await GET("/whoAmI");
+      this.userInfo = data;
+    },
+    async authorize(body) {
+      const { data, error } = await POST("/login", { body });
+
+      if (!error) {
+        const { token } = data;
+
+        setToken(token);
+        this.auth = true;
+        this.setUserInfo();
+      }
+
+      return !error;
+    },
+    cleanUser() {
+      this.userInfo = {};
+      this.auth = false;
+    },
+    async logout() {
+      DELETE("/logout");
+      removeToken();
+      this.cleanUser();
     },
   },
 });
